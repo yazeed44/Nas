fbbd8034c3666228ed82434f49a98ed2a8fc9e6b
Yazeed Almuqwishi <yazeed.almuqwishi@gmail.com>
2023-03-06 14:06:50 +0000

    add placeholder video

--- src/app.js
+++ src/app.js
@@ -2,12 +2,12 @@ const { create } = require("ipfs-http-client");
 
 const ipfsClient = create("http://127.0.0.1:5001/api/v0");
 
+
 const clientName = `User ${Math.ceil(Math.random() * 100)}`;
 
 ipfsClient.pubsub.publish("nas-streaming", `${clientName} just joined`);
 
 // TODO add webcam feed
-// TODO add textbox for publishing messages
 // TODO list all messages
 ipfsClient.pubsub.subscribe("nas-streaming", (message) => {
   const msgString = new TextDecoder().decode(message.data);
